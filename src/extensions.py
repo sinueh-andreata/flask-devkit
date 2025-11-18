9e32f5cb20153e028991253db80ec1cae9eceaea
sinueh-andreata <sinuehnoan@gmail.com>
2025-11-18 16:43:04 -0300

    feat: cors and marshmallow

--- src/extensions.py
+++ src/extensions.py
@@ -3,11 +3,12 @@ from flask_wtf import CSRFProtect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_security import Security
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 db = SQLAlchemy()
-
 csrf = CSRFProtect()
-
 security = Security()
-
+ma = Marshmallow()
+cors = CORS()
 limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
